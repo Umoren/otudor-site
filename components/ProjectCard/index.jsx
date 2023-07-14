e50779de19c5ff23969e192bc068f3c42ae38b09
sammy <samuelumoren365@gmail.com>
2023-07-14 19:07:08 +0000

    fix mobile scroll bug

--- components/ProjectCard/index.jsx
+++ components/ProjectCard/index.jsx
@@ -31,7 +31,7 @@ export default function ProjectCard() {
         <>
             <div className="sm:pt-32 sm:pb-40 -my-8 group md:py-0" data-href="">
                 {githubProjects && githubProjects.map((project, i) => (
-                    <article className="my-4 bg-gray-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" itemScope itemType="http://schema.org/CreativeWork" key={i}>
+                    <article className="my-4 bg-gray-800 md:transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" itemScope itemType="http://schema.org/CreativeWork" key={i}>
                         <Link href={project.repoLink} target="_blank" rel="noopener noreferrer" className="block p-40">
                             <p className="text-xs font-bold tracking-widest uppercase text-white" itemProp="language"> {project.language} </p>
                             <h3 className="my-3 text-lg font-semibold leading-tight text-white" itemProp="title"> {project.title} </h3>
